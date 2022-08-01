5925822c823d4b2684cc8dfddfd1a884feeb1bfc
Karim Ahmed <87246131+rexmirak@users.noreply.github.com>
2022-08-01 07:38:29 +0200

    directory changes7

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
 });
 
 // app.listen(3000);
-const port = process.env.PORT || 8000;
-server.listen(port, () => {
+var port = process.env.PORT || 8000;
+app.listen(port, function () {
   console.log("App is running on port " + port);
 });
