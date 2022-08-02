6881f242d78340d53beea3e5a446ff3f43b9b0ac
Karim Ahmed <87246131+rexmirak@users.noreply.github.com>
2022-08-02 18:52:30 +0200

    front end manage profiles

--- index.js
+++ index.js
@@ -30,9 +30,6 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "html");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: "views" });
-});
 
 // app.listen(3000);
 var port = process.env.PORT || 8000;
@@ -90,6 +87,13 @@ app.post("/", function (req, res) {
     }
   }
 });
+//get pages
+app.get("/", (req, res) => {
+  res.sendFile("BOA-Home.html", { root: "views" });
+});
 app.get("/change-password.html", function (req, res) {
   res.sendFile("change-password.html", { root: "views" });
+});
+app.get("/BOA-Manage-Profiles.html", function (req, res) {
+  res.sendFile("BOA-Manage-Profiles.html", { root: "views" });
 });
